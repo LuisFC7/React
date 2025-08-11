c0570e14d7dd13ddfe50dda7e4500fa432bf20f5
LuisFC7 <luisitofc.28@gmail.com>
2025-08-11 17:56:43 -0600

    Uploads

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -6,10 +6,32 @@ import { db } from "./data/db";
 function App() {
   
   const [data, setData] = useState(db)
+  const [cart, setCart] = useState([])
+
+  function addToCart(item){
+
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    if(itemExists >= 0){
+      const updatedCart = [...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+      
+    }else{
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+  }
+
+  function removeFromCart(id){
+    setCart(prevCart => prevCart.filter(guitar  => guitar.id !== id))
+  }
 
   return (
     <>
-      <Header />
+      <Header 
+        cart={cart}
+        removeFromCart = {removeFromCart}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
@@ -19,6 +41,9 @@ function App() {
               <Guitar
                 key = {guitar.id}
                 guitar = {guitar}
+                cart = {cart}
+                setCart = {setCart}
+                addToCart = {addToCart}
               />
             )
           )}
