d0df2e6b7838a6943c023d3bc32da0474fde0d83
LuisFC7 <luisitofc.28@gmail.com>
2025-08-13 17:54:17 -0600

    Version 1.0

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -4,9 +4,21 @@ import Guitar from "./components/Guitar";
 import { db } from "./data/db";
 
 function App() {
+
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
   
-  const [data, setData] = useState(db)
+  const [data] = useState(db)
   const [cart, setCart] = useState([])
+
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+  
+  useEffect(() => {
+     localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
 
   function addToCart(item){
 
@@ -26,11 +38,47 @@ function App() {
     setCart(prevCart => prevCart.filter(guitar  => guitar.id !== id))
   }
 
+  function increaseQuantity(id){
+    const updatedCart = cart.map(item=>{
+      if(item.id ===id && item.quantity < MAX_ITEMS ){
+        return{
+          ...item,
+          quantity: item.quantity+1
+        }
+      }
+      return item
+    })
+
+    setCart(updatedCart)
+  }
+
+  function decreaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS){
+        return{
+          ...item,
+          quantity:item.quantity-1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  function clearCart(){
+    setCart([])
+  }
+
+
+
   return (
     <>
       <Header 
         cart={cart}
         removeFromCart = {removeFromCart}
+        increaseQuantity = {increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart = {clearCart}
       />
 
       <main className="container-xl mt-5">
